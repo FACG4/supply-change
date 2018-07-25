789f871e36cb2e3dd0b5d8950ea0f777f0986380
marwa <marwajomaa4@gmail.com>
2018-07-25 14:08:43 +0300

    fixing file structure

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
-      </div>
+      <div className="App" />
     );
   }
 }
