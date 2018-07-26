8478beb8cf0a1fe10c361576661026d289a62fdf
ramyalshurafa <ramyshurafa@hotmail.com>
2018-07-26 12:34:43 +0300

    fixing eslint

--- server/index.js
+++ server/index.js
@@ -3,8 +3,7 @@ const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
 
