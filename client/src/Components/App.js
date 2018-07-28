2b29de404017adafbc2f58d3a0a9b994baaeda3e
ramyalshurafa <ramyshurafa@hotmail.com>
2018-07-28 23:09:01 +0300

    fixing navbar position, removing white lines in the code, change the css selector method and converting the anonymous function in window listener into a seperated function

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 import Header from './CommonComponents/Header';
+
 class App extends Component {
   render() {
     return (
--- client/src/Components/CommonComponents/Header/index.js
+++ client/src/Components/CommonComponents/Header/index.js
@@ -2,43 +2,39 @@ import React, { Component } from 'react';
 import './style.css';
 
 class Header extends Component {
+  handleScroll = () => {
+    if (Math.round(window.scrollY) > 30) {
+      document.querySelector('#navbar').classList.add('scrolled');
+      document.querySelector('#main').classList.add('scrolled');
+    } else {
+      document.querySelector('#navbar').classList.remove('scrolled');
+      document.querySelector('#main').classList.remove('scrolled');
+    }
+  }
   componentDidMount() {
-    window.addEventListener('scroll', () => {
-      if (Math.round(window.scrollY) > 30) {
-        document.querySelector('#navbar').classList.add('scrolled');
-        document.querySelector('#main').classList.add('scrolled');
-      } else {
-        document.querySelector('#navbar').classList.remove('scrolled');
-        document.querySelector('#main').classList.remove('scrolled');
-      }
-    });
+    window.addEventListener('scroll', () => this.handleScroll());
   }
   render() {
     return (
       <header id='header'>
         <div className='navbar' id='navbar'>
           <div className='navbar--fixed' >
-
             <ul className='nav'>
               <li className='item'>
                 <a href='#'>How it works</a>
               </li>
-
               <li className='item'>
                 <a href='#'>About us</a>
               </li>
-
               <li className='item'>
                 <div className='post_find_Contract'>
-
                   <a href='#'>Post a contract</a>
                 </div>
               </li>
-
               <li className='item' >
                 <div className=' companyAction'>
-                  <span>
-                    <img src='https://upload.wikimedia.org/wikipedia/commons/6/62/USPHS_Commissioned_Corps_insignia.png' alt='' />
+                  <span className='companyAction__span'>
+                    <img className='companyAction__img' src='https://upload.wikimedia.org/wikipedia/commons/6/62/USPHS_Commissioned_Corps_insignia.png' alt='' />
                       company Name
                     <i className='fas fa-caret-down'></i>
                   </span>
@@ -46,13 +42,10 @@ class Header extends Component {
                     <a href='#'><i className='fas fa-user'></i>Profile</a>
                     <a href='#'><i className='fas fa-plus-circle'></i>Post a contract</a>
                     <a href='#'><i className='fas fa-sign-out-alt'></i>Logout</a>
-
                   </div>
-
                 </div>
               </li>
             </ul>
-
             <div className='logo'>
               <img src='https://pbs.twimg.com/profile_images/996015715722387458/WofTKvoF_400x400.jpg' />
             </div>
@@ -61,9 +54,7 @@ class Header extends Component {
             <h1>Welcome &lt;Company Name&gt;</h1>
           </div>
         </div>
-
       </header>
-
     );
   }
 }
