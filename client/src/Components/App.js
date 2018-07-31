23ca129d2fd62dab3fd9be69e2b585c8e515f1c5
marwa <marwajomaa4@gmail.com>
2018-07-31 13:41:14 +0300

    solving requestd changes relates # 21

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,21 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-
-import Signup from './Pages/Signup';
-import MainInfo from './Pages/SEProfile/MainInfo';
-import Header from './CommonComponents/Header';
-
+import SignUp from  './Pages/Signup'
 
 class App extends Component {
   render() {
     return (
-
       <div className="App">
-        <Header />
-        <main id='main'>
-        </main>
-
+        <SignUp  />
       </div>
     );
   }
