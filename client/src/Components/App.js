49b7e4281153531aaeb2b41899d0159824133860
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-07-31 12:34:08 +0300

    homePage with all components relates #20

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
-
+// import Footer from './CommonComponents/Footer';
+import Testimoinals from './Pages/HomePage/Testimoinals';
+import HowItWork from './Pages/HomePage/HowItWork';
+import HomeHeader from './Pages/HomePage/HomeHeader';
+import HeroSection from './Pages/HomePage/HeroSection';
 class App extends Component {
   render() {
     return (
+      <React.Fragment>
 
-      <div className='App'>
+        <div className='App'>
+          <HomeHeader />
+          <HeroSection />
+          <HowItWork />
+          <Testimoinals/>
+          {/* <Footer /> */}
 
-      </div>
+        </div>
+
+      </React.Fragment>
     );
   }
 }
