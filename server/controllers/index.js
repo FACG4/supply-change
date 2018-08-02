d8b29a1a32cebcbb847ac475056e4c0fb1346afe
MohammedYehia <m-ya@windowslive.com>
2018-08-02 10:15:35 +0300

    changing the name style of the file to snake_case #30

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,6 +1,6 @@
 const express        = require('express');
 const router         = express.Router();
-const getCompanyInfo = require('./getCompanyInfo');
+const getCompanyInfo = require('./get_company_info');
 
 router.get('/companyinfo/:companyNumber', getCompanyInfo);
 
