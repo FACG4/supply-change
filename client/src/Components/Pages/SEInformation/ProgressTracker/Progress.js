740ffa5924f84adc0234c836ecd33134e8a37558
MohammedYehia <m-ya@windowslive.com>
2018-08-03 22:56:58 +0300

    fix spaces #42

--- client/src/Components/Pages/SEInformation/ProgressTracker/Progress.js
+++ client/src/Components/Pages/SEInformation/ProgressTracker/Progress.js
@@ -1,16 +1,15 @@
 import React from 'react';
 
-export default (props) => {
-
-    const {selected, img, title} = props;
-    const className = "progress__circle " + (selected ? "progress__circle-selected" : "");
+export default (props)=> {
+    const { selected, img, title } = props;
+    const className = 'progress__circle ' + (selected ? 'progress__circle-selected' : '');
 
     return(
-        <div className="progress__image-info">
-            <div className={className}>
-                <img className="progress__icon" src={img} alt={img}/>
+        <div className = 'progress__image-info'>
+            <div className = { className }>
+                <img className = 'progress__icon' src = { img } alt = { img }/>
             </div>
-            <p>{title}</p>
+            <p>{ title }</p>
         </div>
     )
 }
