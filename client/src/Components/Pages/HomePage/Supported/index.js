b9b5c34a57035e211f676019a5ab6f660db82b7a
marwa <marwajomaa4@gmail.com>
2018-08-05 22:27:02 +0300

    creating supported section on home page #54

--- client/src/Components/Pages/HomePage/Supported/index.js
+++ client/src/Components/Pages/HomePage/Supported/index.js
@@ -18,25 +18,25 @@ const Supported =() => {
 
         <div className='second'>
           <div className='img__second'>
-            <img src={ second } alt='' />
+            <img src={ second } alt='img__second' />
           </div>
         </div>
 
         <div className='first'>
           <div className='img__first'>
-            <img src={ first } alt='' />
+            <img src={ first } alt='img__first' />
           </div>
         </div>
 
         <div className='third'>
           <div className='img__third'>
-            <img src={ fourth } alt='' className='supported__img' />
+            <img src={ fourth } alt='img__third' className='supported__img' />
           </div>
         </div>
 
         <div className='fourth'>
           <div className='img__fourth'>
-            <img src={ third } alt='' />
+            <img src={ third } alt='img__fourth' />
           </div>
         </div>
 
