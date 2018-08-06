679ee442e68fb2f3d1b9a503ee3967c2979fec32
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-08-06 11:22:19 +0300

    adding scprits to deploying on Heroku

--- client/src/Components/Pages/HomePage/Testimoinals/index.js
+++ client/src/Components/Pages/HomePage/Testimoinals/index.js
@@ -3,7 +3,7 @@ import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import test from './test.svg';
-import './Style.css';
+import './style.css';
 
 const settings = {
   dots: true,
--- server/database/db_build.js
+++ server/database/db_build.js
@@ -1,17 +1,17 @@
-const dbcon = require('./db_connection.js');
-const fs = require('fs');
-
-const sql = fs.readFileSync(`${__dirname}/db.sql`).toString();
-const dbConnect = (cb) => {
-    dbcon.query(sql, (err, res) => {
-        if (err) return cb(err);
-        cb(null, res);
-    });
-};
-
-dbConnect((err, result) => {
-    if(err) return console.log(err);
-    return console.log('Supply-Change database has been initialized');
-});
-
-module.exports = dbConnect;
+// const dbcon = require('./db_connection.js');
+// const fs = require('fs');
+//
+// const sql = fs.readFileSync(`${__dirname}/db.sql`).toString();
+// const dbConnect = (cb) => {
+//     dbcon.query(sql, (err, res) => {
+//         if (err) return cb(err);
+//         cb(null, res);
+//     });
+// };
+//
+// dbConnect((err, result) => {
+//     if(err) return console.log(err);
+//     return console.log('Supply-Change database has been initialized');
+// });
+//
+// module.exports = dbConnect;
