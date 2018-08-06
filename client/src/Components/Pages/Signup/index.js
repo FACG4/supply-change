4066b83cfc7bf53b1641193a7dc3aded014f7978
marwa <marwajomaa4@gmail.com>
2018-08-06 11:02:43 +0300

    creating signup backend function

--- client/src/Components/Pages/Signup/index.js
+++ client/src/Components/Pages/Signup/index.js
@@ -1,28 +1,91 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Redirect } from 'react-router-dom';
 import './style.css';
+import axios from 'axios';
 import Fontawesome from 'react-fontawesome';
 import family from './Imgs/family.png';
 import Group1 from './Imgs/Group1.png';
 
 class SignUp extends Component {
-  state={
-    userInfo:{}
+  state = {
+    userInfo:{},
+    companyInfo:[],
+    visible:false,
+    error:'',
+    redirect:false
   }
 
-  handleChange = (e)=>{
+  isCompany = (e)=> {
+    e.preventDefault();
     const { userInfo } = this.state;
-    userInfo[e.target.name]=e.target.value;
-    this.setState({ userInfo })
+    const newUserInfo = {...userInfo}
+    const companyNumber = userInfo.CompanyHouseNumber;
+    newUserInfo[e.target.name]= companyNumber ;
+    console.log(companyNumber,'companyNumber')
+
+      axios.get(`/companyinfo/${companyNumber}`)
+      .then(result =>{
+        console.log(result,'result');
+        if(result.data.errors){
+          console.log(result.data.errors[0].error,'errror');
+          this.setState({error:result.data.errors[0].error})
+          console.log(this.state.error,'error from state');
+        }else {
+          this.setState({companyInfo:result.data,
+                visible:!this.state.visible,
+                error:''})
+
+          console.log(this.state.companyInfo);
+        }
+      })
+      .catch(err=>console.log(err, ' err'))
   }
+
+ submitForm =(e)=>{
+   e.preventDefault();
+   console.log('ramy');
+   const { userInfo } = this.state;
+   userInfo.companyName = this.state.companyInfo.company_name;
+   console.log(userInfo)
+   if(userInfo['T&C']){
+     axios.post('/userdata',userInfo)
+     .then(function (response) {
+       console.log(response);
+       if(response.data.msg === 'success'){
+         window.location = '/login'
+       }
+     })
+     .catch(function (error) {
+       console.log(error);
+     })
+   }
+}
+
+  handleChange = (e)=>{
+    e.preventDefault();
+    const { userInfo } = this.state;
+    const newUserInfo = {...userInfo}
+    if(e.target.type === 'checkbox'){
+      newUserInfo[e.target.name] = e.target.checked
+    }else{
+      newUserInfo[e.target.name]= e.target.value ;
+    }
+    this.setState({ userInfo: newUserInfo });
+    console.log( userInfo,'userinfo' );
+}
   render() {
     return (
-      <div>
+      this.state.redirect ?
+        <Redirect to='/' /> :
+        (
+      <div className="signup__main">
       <div className="signup__topForm">
           <h3 className="header1">
               Sign Up
           </h3>
           <div className="select-user">
+
             <button className="signup__button button0">
                Buyer
             </button>
@@ -39,15 +102,25 @@ class SignUp extends Component {
             <form action="" method="POST" className="form Signup">
 
               <div className="form__field">
-                <input id="CompanyHouseNumber" type="text" name="CompanyHouseNumber" onChange={this.handleChange} className="form__input" placeholder="Company House Number" required />
+                <input id="CompanyHouseNumber" type="number" min="0" name="CompanyHouseNumber" onChange={this.handleChange} className="form__input" placeholder="Company House Number" required />
                 <label htmlFor="CompanyHouseNumber">
                   <Fontawesome className="fontawesome" name="sort-numeric-up" />
                   <span className="hidden" />
                 </label>
               </div>
 
+                {!this.state.visible?
               <div className="form__field">
-                <input id="businessName" type="text" name="businessName"  onChange={this.handleChange} className="form__input" placeholder="Business Name" required />
+                <input type="submit" className="submitButton" value="check" onClick={this.isCompany} />
+              </div>
+              :null
+            }
+              {this.state.error?<div className="error-msg">Company Not Found</div>:null}
+
+            {this.state.visible?(
+              <div>
+              <div className="form__field">
+                <input id="businessName" type="text" name="businessName" value={this.state.companyInfo.company_name}  className="form__input" placeholder="Business Name" required />
                 <label htmlFor="businessName">
                   <Fontawesome className="fontawesome" name="briefcase" />
                   <span className="hidden" />
@@ -72,7 +145,7 @@ class SignUp extends Component {
               </div>
 
               <div className="form__field">
-                <input id="email" type="text" name="eamil"  onChange={this.handleChange} className="form__input" placeholder="Email" required />
+                <input id="email" type="email" name="eamil"  onChange={this.handleChange} className="form__input" placeholder="Email" required="required" />
                 <label htmlFor="email">
                   <Fontawesome className="fontawesome" name="envelope" />
                   <span className="hidden" />
@@ -90,14 +163,14 @@ class SignUp extends Component {
               <div className="form__field">
                 <input id="confirm" type="password" name="confirm"   onChange={this.handleChange} className="form__input" placeholder="Confirm Password" required />
                 <label htmlFor="confirm">
-                  <Fontawesome className="fontawesome" name="unlock-alt" />
+                  <Fontawesome className="fontawesome" name="undo-alt" />
 
                   <span className="hidden" />
                 </label>
               </div>
 
               <div className="form__field">
-                <input id="phone" type="text" name="phone"  onChange={this.handleChange} className="form__input" placeholder="Phone Number" required />
+                <input id="phone" type="number" name="phone" min="0" onChange={this.handleChange} className="form__input" placeholder="Phone Number" required />
                 <label htmlFor="phone">
                   <Fontawesome className="fontawesome" name="phone" />
                   <span className="hidden" />
@@ -110,19 +183,21 @@ class SignUp extends Component {
               </div>
 
               <div className="checkBox">
-                <input id="mailingList" type="checkbox" name="mailingList" value="mailingList" />
+                <input id="mailingList" type="checkbox" onChange = {this.handleChange} name="mailingList" value="mailingList" />
                 <span className="checkBoxSpan">
                    Join Mailing List
                 </span>
-                <input id="T&C" type="checkbox" name="T&C" value="T&C" checked />
+                <input id="T&C" type="checkbox" onChange = {this.handleChange} name="T&C" value="T&C" />
                 <span className="checkBoxSpan">
                    T&C
                 </span>
               </div>
 
               <div className="form__field">
-                <input type="submit" className="submitButton"value="Sign Up" />
+                <input type="submit" className="submitButton"value="Sign Up" onClick={this.submitForm} />
               </div>
+              </div>
+            ):null}
             </form>
 
             <div className="SubmitDiv">
@@ -139,6 +214,7 @@ class SignUp extends Component {
           </div>
         </div>
       </div>
+    )
     );
   }
 }
--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const search = require('./search');
-
-const getCompanyInfo = require('./get_company_info');
-
 const router = express.Router();
+const getCompanyInfo = require('./get_company_info');
+const storeData = require('./store_user_data');
 
 router.get('/companyinfo/:companyNumber', getCompanyInfo);
-router.post('/search', search);
+router.post('/userdata', storeData);
 
 module.exports = router;
