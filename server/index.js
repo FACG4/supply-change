3baf76a97a5cc322698e5550d0fa15b6dc35001a
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-08-06 15:05:28 +0300

    update cors origin

--- server/index.js
+++ server/index.js
@@ -4,6 +4,12 @@ const app = express();
 const controllers = require('./controllers');
 const bodyParser = require('body-parser');
 
+app.use((req,res,next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
