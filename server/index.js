9888b86f2329bd72f2fccc50e82ba9ea68d68399
haneenshahwan4 <haneenshahwan@hotmail.com>
2018-08-06 15:57:25 +0300

    adding presentation contaner to SEinf comp

--- server/index.js
+++ server/index.js
@@ -5,12 +5,6 @@ const controllers = require('./controllers');
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 9000;
 
-app.use((req,res,next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  next();
-});
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
