1f635e70c4f16113b070332f6533135abf534528
marwa <marwajomaa4@gmail.com>
2018-08-06 11:04:42 +0300

    creating signup backend function

---
+++ server/controllers/store_user_data.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs');
+const query = require('../database/query/query');
+
+module.exports = (req, res) => {
+  console.log(req.body);
+  const { CompanyHouseNumber,companyName,firstName,lastName,eamil,password,phone,mailingList } = req.body;
+  bcrypt.hash(password, 8, (err, hashedPassword) => {
+    console.log(hashedPassword);
+    if (err) return res.end(JSON.stringify({ err: 'err at hashing password' }));
+    const sql = {
+      text: 'INSERT INTO social_enterprise_basic(SE_name, SE_house_NO,first_name, last_name, email, phone_number,mailing_list, password) VALUES ($1,$2,$3,$4,$5,$6,$7,$8)',
+      values: [companyName,CompanyHouseNumber,firstName,lastName,eamil,phone, mailingList, hashedPassword]
+    };
+    query(sql).then(
+      res.end(JSON.stringify({ msg: 'success' }))
+    ).catch(err => {
+      res.end(JSON.stringify({ err: 'err in inserting the data' }));
+    });
+  });
+};
