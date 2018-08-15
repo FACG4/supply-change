b8633fcd476139e5fadc57ba9aecce7d2f56a222
ramyalshurafa <ramyshurafa@hotmail.com>
2018-08-15 23:33:38 +0300

    fix navbar in home page

--- client/src/Components/Pages/HomePage/HomeHeader/index.js
+++ client/src/Components/Pages/HomePage/HomeHeader/index.js
@@ -4,20 +4,30 @@ import { NavLink, Redirect } from 'react-router-dom';
 import './style.css';
 import logo from './logo.svg';
 
-const HomeHeader = () => (
-  <header>
-    <div className='navBar'>
-      <div className='logoDiv'>
-        <img src={logo} alt=''/>
+const HomeHeader = () => {
+  const info = JSON.parse(localStorage.getItem('userInfo'));
+  const options =info ? {
+    path: '/profile',
+    text: 'Profile'
+  } : {
+    path: '/login',
+    text: 'Login'
+  };
+  return (
+    <header>
+      <div className='navBar'>
+        <div className='logoDiv'>
+          <img src={logo} alt=''/>
+        </div>
+        <div className='textNavBar'>
+          <NavLink to='/contract/post'>Post a contract</NavLink>
+          <span>|</span>
+          <NavLink to='/about'>About</NavLink>
+          <span>|</span>
+          <NavLink to={options.path}>{options.text}</NavLink>
+        </div>
       </div>
-      <div className='textNavBar'>
-        <NavLink to='/contract/post'>Post a contract</NavLink>
-        <span>|</span>
-        <NavLink to='/about'>About</NavLink>
-        <span>|</span>
-        <NavLink to='/login'>login</NavLink>
-      </div>
-    </div>
-  </header>);
+    </header>);
+};
 
 export default HomeHeader;
